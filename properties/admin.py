50c47ab5d341d4673312827caa94058da7473230
Chinedu Aja <Chineduaja26@gmail.com>
2023-08-17 13:19:26 +0100

    Second Commit

--- properties/admin.py
+++ properties/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import Property, PropertyImage, Amenity
 
-# Register your models here.
-admin.site.register(Property)
+
+class PropertyImageInline(admin.TabularInline):
+    model = PropertyImage
+
+
+class AmenityInline(admin.TabularInline):
+    model = Amenity
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    inlines = [PropertyImageInline, AmenityInline]
+
+
 admin.site.register(PropertyImage)
 admin.site.register(Amenity)
